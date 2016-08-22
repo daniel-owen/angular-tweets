65a1f0d3fd0957b3a93d6272d5c237bcb1f2f0e0
daniel owen <dowen35@gmail.com>
2016-08-22 19:09:51 -0400

    4th commit

--- js/homecontroller.js
+++ js/homecontroller.js
@@ -1,3 +1,39 @@
-tweetApp.controller('homeController', function($scope, $http){
+tweetApp.controller('homeController', function($scope, $http, $location, $mdDialog, $mdMedia){
 	$scope.homePage = true;
+	
+	$scope.$watch(function(){
+		return $location.path();
+	},
+	function(newPath){
+		$scope.trump = "";
+		$scope.hillary = "";
+		$scope.home = "";
+		console.log("The path changed. It's now " + newPath);
+		if(newPath == "/trump"){
+			$scope.trump = "active";
+		}else if(newPath == "/hillary"){
+			$scope.hillary = "active"
+		}else if(newPath == "/"){
+			$scope.home = "active";
+		}
+	}
+	);
+
+	$scope.showPrompt = function(ev) {
+		// Appending dialog to document.body to cover sidenav in docs app
+		var confirm = $mdDialog.prompt()
+			.title('Search recent tweets regarding presidential candidates')
+			.textContent('Trump? Hillary?')
+			.placeholder('Candidate')
+			.ariaLabel('Candidate name')
+			.targetEvent(ev)
+			.ok('Display Tweets')
+			.cancel('I don\'t care');
+		$mdDialog.show(confirm).then(function(result) {
+			$scope.status = 'Search recent tweets regarding: ' + result;
+		}, function() {
+			$scope.status = 'You didn\'t name your dog.';
+		});
+	};
+
 });
--- js/tweetController.js
+++ js/tweetController.js
@@ -12,47 +12,17 @@ tweetApp.controller('tweetController', function($scope, $http, $location, $route
 		var searchTerm = 'trump';
 	}
 
-	// $scope.getTweets = function(searchText){
-		$http({
-			method: 'GET',
-			url: 'http://digitalcrafts.com/students/twitter/hashtag.php?user=true&hash=' + searchTerm
-		}).then(
-			function successFunction(tweetData){
-				$scope.searchTweets = tweetData.data.statuses;
-				console.log(tweetData.data.statuses);
-			},function failureFunction(tweetData){
-				console.log(tweetData);
-			}
-		);
-	// }
-
-	$scope.getTrumpTweets = function(){
-		$http({
-			method: 'GET',
-			url: 'http://digitalcrafts.com/students/twitter/hashtag.php?user=true&hash=donald%20trump'
-		}).then(
-			function successFunction(tweetData){
-				$scope.trumpTweets = tweetData.data.statuses;
-				console.log(tweetData);
-			},function failureFunction(tweetData){
-				console.log(tweetData);
-			}
-		);
-	}
-
-	$scope.getHillaryTweets = function(){
-		$http({
-			method: 'GET',
-			url: 'http://digitalcrafts.com/students/twitter/hashtag.php?user=true&hash=hillary%20clinton'
-		}).then(
-			function successFunction(tweetData){
-				$scope.hillaryTweets = tweetData.data.statuses;
-				console.log(tweetData);
-			},function failureFunction(tweetData){
-				console.log(tweetData);
-			}
-		);
-	}
+	$http({
+		method: 'GET',
+		url: 'http://digitalcrafts.com/students/twitter/hashtag.php?user=true&hash='+searchTerm
+	}).then(
+		function successFunction(tweetData){
+			$scope.searchTweets = tweetData.data.statuses;
+			console.log(tweetData.data.statuses);
+		},function failureFunction(tweetData){
+			console.log(tweetData);
+		}
+	);
 
 });
 
@@ -80,3 +50,32 @@ tweetApp.controller('tweetController', function($scope, $http, $location, $route
 // 	}
 
 // });
+
+
+	// $scope.getHillaryTweets = function(){
+	// 	$http({
+	// 		method: 'GET',
+	// 		url: 'http://digitalcrafts.com/students/twitter/hashtag.php?user=true&hash=hillary%20clinton'
+	// 	}).then(
+	// 		function successFunction(tweetData){
+	// 			$scope.hillaryTweets = tweetData.data.statuses;
+	// 			console.log(tweetData);
+	// 		},function failureFunction(tweetData){
+	// 			console.log(tweetData);
+	// 		}
+	// 	);
+	// }
+
+	// $scope.getTrumpTweets = function(){
+	// 	$http({
+	// 		method: 'GET',
+	// 		url: 'http://digitalcrafts.com/students/twitter/hashtag.php?user=true&hash=donald%20trump'
+	// 	}).then(
+	// 		function successFunction(tweetData){
+	// 			$scope.trumpTweets = tweetData.data.statuses;
+	// 			console.log(tweetData);
+	// 		},function failureFunction(tweetData){
+	// 			console.log(tweetData);
+	// 		}
+	// 	);
+	// }
